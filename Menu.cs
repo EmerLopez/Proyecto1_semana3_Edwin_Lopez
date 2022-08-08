3c9f99d0832072bfcda5f7d5ab826f7cfdf6c2a8
Edwin <usss017222@ugb.edu.sv>
2022-08-08 11:44:04 -0600

    Modificamos la clase menu

--- Menu.cs
+++ Menu.cs
@@ -6,8 +6,43 @@ using System.Threading.Tasks;
 
 namespace Proyecto1_semana_3
 {
-    class Menu
+    class menu
     {
+        static void Main(string[] args)
+        {
 
+            string opcion;
+            int opcionint;
+
+            System.Console.WriteLine("Menu Principal de la Practica 1: ");
+            System.Console.WriteLine("Selecciona tu opcion: ");
+            opcion = Console.ReadLine();
+            opcionint = Int32.Parse(opcion);
+
+
+            switch (opcionint)
+            {
+                case 1:
+                    System.Console.WriteLine("Aqui llamo el programa1");
+                    break;
+
+                case 2:
+                    System.Console.WriteLine("Aqui llamo el programa2");
+                    break;
+
+                case 3:
+                    System.Console.WriteLine("Aqui llamo el programa3");
+                    break;
+
+                case 4:
+                    System.Console.WriteLine("Opcion no disponible");
+                    break;
+
+            }
+
+
+            System.Console.ReadKey();
+        }
     }
+
 }
