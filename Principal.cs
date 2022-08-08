25c07b1c67698330186fd8609c7b9addea1ed37a
Edwin <usss017222@ugb.edu.sv>
2022-08-08 11:51:57 -0600

    Nuevos cambios en funciones

--- Principal.cs
+++ Principal.cs
@@ -10,7 +10,28 @@ namespace Proyecto1_semana_3
     {
         static void Main(string[] args)
         {
+            System.Console.WriteLine("Programa Principal");
+            System.Console.WriteLine("Primer Programa");
 
+            System.Console.ReadKey();
+
+            int repetir;
+
+
+            menu_interno();
+
+
+            System.Console.WriteLine("Ingrese la opcion: ");
+            repetir = Int32.Parse(Console.ReadLine());
+
+
+            while (repetir == 1)
+            {
+                menu_interno();
+
+
+
+            }
         }
         static void menu_interno()
         {
@@ -18,23 +39,23 @@ namespace Proyecto1_semana_3
         }
         static void programa_if()
         {
-
+            System.Console.WriteLine("Aqui llamo el ejemplo if");
         }
         static void programa_switch()
         {
-
+            System.Console.WriteLine("Aqui llamo el ejemplo switch");
         }
         static void programa_for()
         {
-
+            System.Console.WriteLine("Aqui llamo el ejemplo for");
         }
         static void programa_while()
         {
-
+            System.Console.WriteLine("Aqui llamo el ejemplo while");
         }
         static void programa_dowhile()
         {
-
+            System.Console.WriteLine("Aqui llamo el ejemplo dowhile");
         }
     }
     
